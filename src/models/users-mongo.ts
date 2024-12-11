7b12c0c5324293b10863daabd2a7774e10e3236b
David Castellar Martínez <castellardav@gmail.com>
2024-12-11 09:56:18 -0500

    feat: add hash to the password before saving it in the db

--- src/models/users-mongo.ts
+++ src/models/users-mongo.ts
@@ -1,4 +1,5 @@
-import { Schema, model } from "mongoose";
+import { CallbackError, Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
 
 interface IUser {
   name: string;
@@ -29,6 +30,26 @@ const userSchema = new Schema<IUser>({
     type: Number,
     required: true,
   },
+});
+
+// Before saving the user, hash the password
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+    return next();
+  } catch (error) {
+    return next(error as CallbackError);
+  }
 });
 
 export const Users = model<IUser>("User", userSchema);
